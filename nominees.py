e48e04009bca9d24c81362e567e341c93930581f
input null <Dehyrf@users.noreply.github.com>
2016-02-09 10:16:50 -0600

    Update nominees.py

--- nominees.py
+++ nominees.py
@@ -1,3 +1,52 @@
+from html.parser import HTMLParser  
+from urllib.request import urlopen  
+from urllib import parse
+
+########################################
+#Define before running:
+base_sites = ['http://www.slashfilm.com/', 'http://www.collider.com/', 'http://www.joblo.com/', 'http://www.geektyrant.com/', 'http://www.screencrush.com/', 'http://www.ign.com/movies', 'http://www.cinemablend.com/', 'http://www.denofgeek.us/#switched', 'http://www.guardian.co.uk/film', 'http://movies.yahoo.com/', 'http://www.screenjunkies.com/', 'http://www.filmschoolrejects.com/', 'http://www.screenrant.com/', 'http://www.darkhorizons.com/']
+########################################
+
+class Oscrawl(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag  'a':
+            for (key, value) in attrs:
+                if key  'href':
+                    newUrl = parse.urljoin(self.baseUrl, value)
+                    self.links = self.links + [newUrl]
+
+    def getLinks(self, url):
+        self.links = []
+        self.baseUrl = url
+        response = urlopen(url)
+        if response.getheader('Content-Type')=='text/html':
+            htmlBytes = response.read()
+            htmlString = htmlBytes.decode("utf-8")
+            self.feed(htmlString)
+            return htmlString, self.links
+        else:
+            return "",[]
+
+def spider(url, words, maxPages):  
+    pagesToVisit = [url]
+    numberVisited = 0
+    foundWord = False
+    while numberVisited < maxPages and pagesToVisit != [] and not foundWord:
+        numberVisited = numberVisited +1
+        url = pagesToVisit[0]
+        pagesToVisit = pagesToVisit[1:]
+        try:
+            print(numberVisited, "Visiting:", url)
+            parser = LinkParser()
+            data, links = parser.getLinks(url)
+            if data.find(word)>-1:
+                foundWord = True
+                pagesToVisit = pagesToVisit + links
+                return True
+        except:
+            return False
+
+
 BEST_PICTURE = [The Big Short, Bridge of Spies, Brooklyn, Mad Max: Fury Road, The Martian, The Revenant, Room, Spotlight]
 
 BEST_ACTOR = [Bryan Cranston Trumbo, Matt Damon The Martian, Leonardo DiCaprio The Revenant, Michael Fassbender Steve Jobs, Eddie Redmayne The Danish Girl]
@@ -41,3 +90,4 @@ ADAPTED_SCREENPLAY = [The Big Short, Brooklyn, Carol, The Martian, Room]
 ORIGINAL_SCREENPLAY = [Bridge of Spies, Ex Machina, Inside Out, Spotlight, Straight Outta Compton]
 
 BEST_CINEMATOGRAPHY = [Carol, The Hateful Eight, Mad Max Fury Road, The Revenant, Sicario]
+
